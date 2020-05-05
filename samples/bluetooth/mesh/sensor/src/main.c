c1a0494e6628238308cb7e3aa50f07d09aa73a5b
Erik Robstad <Erik.Robstad@nordicsemi.no>
2020-05-05 11:36:39 +0200

    Sensor reading now working (without Mesh involved)

--- samples/bluetooth/mesh/sensor/src/main.c
+++ samples/bluetooth/mesh/sensor/src/main.c
@@ -11,33 +11,30 @@
 #include <bluetooth/mesh/models.h>
 #include <bluetooth/mesh/dk_prov.h>
 #include <dk_buttons_and_leds.h>
-#include "model_handler.h"
+
+static struct device *dev;
+static struct sensor_value sensor_val;
 
 void main(void)
 {
-	int err;
-
 	dk_leds_init();
 	dk_buttons_init(NULL);
 
-	err = bt_enable(NULL);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+	dev = device_get_binding(DT_INST_0_NORDIC_NRF_TEMP_LABEL);
+
+	if (dev == NULL) {
+		printk("Could not get device\n");
 		return;
 	}
 
-	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
-	if (err) {
-		printk("Initializing mesh failed (err %d)\n", err);
-		return;
+	printk("*** Sensor reading test - Device: %s ***\n", dev->config->name);
+
+	while (true) {
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_DIE_TEMP, &sensor_val);
+
+		printk("*** Sensor value: %d.%02d ***\n", sensor_val.val1, sensor_val.val2);
+
+		k_sleep(K_MSEC(1000));
 	}
-
-	if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
-	}
-
-	/* This will be a no-op if settings_load() loaded provisioning info */
-	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
-
-	printk("Mesh initialized\n");
 }
