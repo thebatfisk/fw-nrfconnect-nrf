613ee6975f8b3b626cdfc528a7a5f96ac5415649
Erik Robstad <Erik.Robstad@nordicsemi.no>
2020-09-23 13:59:03 +0200

    Add provisioning and basic auto configuring
    Signed-off-by: Erik Robstad <Erik.Robstad@nordicsemi.no>

--- samples/bluetooth/mesh/gateway/src/main.c
+++ samples/bluetooth/mesh/gateway/src/main.c
@@ -5,11 +5,66 @@
  */
 
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/mesh/models.h>
-#include <bluetooth/mesh/dk_prov.h>
+#include <bluetooth/mesh.h>
+#include <settings/settings.h>
+// #include <bluetooth/mesh/models.h>
+// #include <bluetooth/mesh/dk_prov.h>
 #include <dk_buttons_and_leds.h>
+#include "gw_provisioning.h"
+
+K_SEM_DEFINE(sem_button, 0, 1);
+
+enum modes
+{
+	m_provision = 0,
+	m_configure,
+	m_testing,
+};
+
+static int mode = -1;
+
+static void button_handler(uint32_t pressed, uint32_t changed)
+{
+	if ((pressed & BIT(0))) {
+		mode = m_provision;
+		k_sem_give(&sem_button);
+	} else if ((pressed & BIT(1))) {
+		mode = m_configure;
+		k_sem_give(&sem_button);
+	} else if ((pressed & BIT(2))) {
+		mode = m_testing;
+		k_sem_give(&sem_button);
+	}
+}
 
 void main(void)
 {
-	printk("Bluetooth Mesh Gateway\n");
+	int err;
+
+	printk("Bluetooth Mesh Gateway initializing...\n");
+
+	dk_buttons_init(button_handler);
+
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	bt_ready();
+
+	while (1) {
+		k_sem_take(&sem_button, K_FOREVER);
+
+		if (mode == m_provision) {
+			provision_device(15);
+		} else if (mode == m_configure) {
+			configure_device();
+		} else if (mode == m_testing) {
+			testing();
+		}
+	}
 }
