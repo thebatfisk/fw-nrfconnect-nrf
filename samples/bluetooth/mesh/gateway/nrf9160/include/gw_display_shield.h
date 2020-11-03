9e430131d426fac75d9357d3949308a6ee8ca741
Erik Robstad <Erik.Robstad@nordicsemi.no>
2020-11-03 11:56:55 +0100

    Added files from Erik dev branch
    Signed-off-by: Erik Robstad <Erik.Robstad@nordicsemi.no>

---
+++ samples/bluetooth/mesh/gateway/nrf9160/include/gw_display_shield.h
@@ -0,0 +1,180 @@
+#ifndef GW_DISPLAY_SHIELD_H__
+#define GW_DISPLAY_SHIELD_H__
+
+#include <zephyr.h>
+#include "gw_display_mcp23017.h"
+
+// commands
+#define LCD_CLEARDISPLAY 0x01 //!< Clear display, set cursor position to zero
+#define LCD_RETURNHOME 0x02 //!< Set cursor position to zero
+#define LCD_ENTRYMODESET 0x04 //!< Sets the entry mode
+#define LCD_DISPLAYCONTROL                                                     \
+	0x08 //!< Controls the display; allows you to do stuff like turn it on and off
+#define LCD_CURSORSHIFT 0x10 //!< Lets you move the cursor
+#define LCD_FUNCTIONSET 0x20 //!< Used to send the function set to the display
+#define LCD_SETCGRAMADDR                                                       \
+	0x40 //!< Used to set the CGRAM (character generator RAM)
+#define LCD_SETDDRAMADDR 0x80 //!< Used to set the DDRAM (display data RAM)
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00 //!< Used to set text to flow from right to left
+#define LCD_ENTRYLEFT 0x02 //!< Used to set text to flow from left to right
+#define LCD_ENTRYSHIFTINCREMENT                                                \
+	0x01 //!< Used to 'right justify' text from the cursor
+#define LCD_ENTRYSHIFTDECREMENT                                                \
+	0x00 //!< USed to 'left justify' text from the cursor
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04 //!< Turns the display on
+#define LCD_DISPLAYOFF 0x00 //!< Turns the display off
+#define LCD_CURSORON 0x02 //!< Turns the cursor on
+#define LCD_CURSOROFF 0x00 //!< Turns the cursor off
+#define LCD_BLINKON 0x01 //!< Turns on the blinking cursor
+#define LCD_BLINKOFF 0x00 //!< Turns off the blinking cursor
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08 //!< Flag for moving the display
+#define LCD_CURSORMOVE 0x00 //!< Flag for moving the cursor
+#define LCD_MOVERIGHT 0x04 //!< Flag for moving right
+#define LCD_MOVELEFT 0x00 //!< Flag for moving left
+
+// flags for function set
+#define LCD_8BITMODE 0x10 //!< LCD 8 bit mode
+#define LCD_4BITMODE 0x00 //!< LCD 4 bit mode
+#define LCD_2LINE 0x08 //!< LCD 2 line mode
+#define LCD_1LINE 0x00 //!< LCD 1 line mode
+#define LCD_5x10DOTS 0x04 //!< 10 pixel high font mode
+#define LCD_5x8DOTS 0x00 //!< 8 pixel high font mode
+
+#define BUTTON_UP 0x08 //!< Up button
+#define BUTTON_DOWN 0x04 //!< Down button
+#define BUTTON_LEFT 0x10 //!< Left button
+#define BUTTON_RIGHT 0x02 //!< Right button
+#define BUTTON_SELECT 0x01 //!< Select button
+
+/*!
+* @brief Init RGB LCD shield
+*/
+void display_shield_init(void);
+
+/*!
+* @brief Starts I2C connection with display
+* @param cols Sets the number of columns
+* @param rows Sets the number of rows
+* @param charsize Sets the character size
+* @return Returns true if the connection was successful
+*/
+void display_begin(uint8_t cols, uint8_t rows, uint8_t charsize);
+
+/*!
+* @brief High-level command to clear the display
+*/
+void display_clear(void);
+/*!
+* @brief High-level command to set the cursor position to zero
+*/
+void display_home(void);
+/*!
+* @brief High-level command to set the cursor position
+* @param col Column
+* @param row Row
+*/
+void display_set_cursor(uint8_t col, uint8_t row);
+/*!
+* @brief High-level command to turn the display off
+*/
+void display_off(void);
+/*!
+* @brief High-level command to turn the display on
+*/
+void display_on(void);
+/*!
+* @brief High-level command to turn off the blinking cursor off
+*/
+void display_blink_off(void);
+/*!
+* @brief High-level command to turn on the blinking cursor
+*/
+void display_blink_on(void);
+/*!
+* @brief High-level command to turn the underline cursor off
+*/
+void display_cursor_off(void);
+/*!
+* @brief High-level command to turn the underline cursor on
+*/
+void display_cursor_on(void);
+/*!
+* @brief High-level command to scroll display left without changing the RAM
+*/
+void display_scroll_display_left(void);
+/*!
+* @brief High-level command to scroll display right without changing the RAM
+*/
+void display_scroll_display_right(void);
+/*!
+* @brief High-level command to make text flow right to left
+*/
+void display_left_to_right(void);
+/*!
+* @brief High-level command to make text flow left to right
+*/
+void display_right_to_left(void);
+/*!
+* @brief High-level command to 'right justify' text from cursor
+*/
+void display_autoscroll(void);
+/*!
+* @brief High-level command to 'left justify' text from cursor
+*/
+void display_no_autoscroll(void);
+
+/*!
+* @brief High-level command to set the backlight, only if the LCD backpack is
+* used
+* @param status Status to set the backlight
+*/
+void display_set_backlight(uint8_t status);
+
+/*!
+* @brief High-level command that creates custom characters in CGRAM
+* @param location Location in cgram to fill
+* @param charmap[] Character map to use
+*/
+void display_create_char(uint8_t, uint8_t[]);
+
+/*!
+* @brief Write a character to the display
+* @param character The character
+*/
+void display_write_char(char character);
+/*!
+* @brief Write a string to the display
+* @param str The string
+*/
+void display_write_string(const char *str);
+/*!
+* @brief Write a number to the display
+* @param num The number
+*/
+void display_write_number(uint16_t num);
+
+/*!
+* @brief Mid-level command that sends data to the display
+* @param value Data to send to the display
+*/
+void display_write(uint8_t value);
+
+/*!
+* @brief Sends command to display
+* @param value Command to send
+*/
+void command(uint8_t);
+
+/*!
+* @brief reads the buttons from the shield
+* @return Returns what buttons have been pressed
+*/
+uint8_t display_read_buttons(void);
+
+#endif /* GW_DISPLAY_SHIELD_H__ */
---
+++ samples/bluetooth/mesh/gateway/nrf9160/src/gw_display_mcp23017.c
@@ -0,0 +1,162 @@
+/***************************************************
+  This is a library for the MCP23017 i2c port expander
+
+  These displays use I2C to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <zephyr.h>
+#include <drivers/i2c.h>
+#include "gw_display_mcp23017.h"
+
+struct device *i2c_dev;
+
+void mcp23017_init(void)
+{
+	uint8_t reset_a_inputs[2] = { MCP23017_IODIRA, 0xFF };
+	uint8_t reset_b_inputs[2] = { MCP23017_IODIRB, 0xFF };
+
+	i2c_dev = device_get_binding(DT_PROP(DT_NODELABEL(i2c0), label));
+
+	i2c_write(i2c_dev, reset_a_inputs, 2, MCP23017_ADDRESS);
+	i2c_write(i2c_dev, reset_b_inputs, 2, MCP23017_ADDRESS);
+}
+
+void mcp23017_pin_configure(uint8_t p, uint8_t d)
+{
+	uint8_t iodir;
+	uint8_t iodiraddr;
+	uint8_t buf[2];
+
+	if (p > 15) {
+		return;
+	}
+
+	if (p < 8) {
+		iodiraddr = MCP23017_IODIRA;
+	} else {
+		iodiraddr = MCP23017_IODIRB;
+		p -= 8;
+	}
+
+	i2c_write_read(i2c_dev, MCP23017_ADDRESS, &iodiraddr, 1, &iodir, 1);
+
+	if (d == INPUT) {
+		iodir |= 1 << p;
+	} else {
+		iodir &= ~(1 << p);
+	}
+
+	buf[0] = iodiraddr;
+	buf[1] = iodir;
+	i2c_write(i2c_dev, buf, 2, MCP23017_ADDRESS);
+}
+
+uint16_t mcp23017_read_gpio_ab()
+{
+	uint8_t wr_buf = MCP23017_GPIOA;
+	uint8_t rd_buf[2];
+
+	i2c_write_read(i2c_dev, MCP23017_ADDRESS, &wr_buf, 1, rd_buf, 2);
+
+	return rd_buf[1] <<= 8 | rd_buf[0];
+}
+
+void mcp23017_write_gpio_ab(uint16_t ba)
+{
+	uint8_t buf[3];
+
+	buf[0] = MCP23017_GPIOA;
+	buf[1] = (ba & 0xFF);
+	buf[2] = (ba >> 8);
+	i2c_write(i2c_dev, buf, 3, MCP23017_ADDRESS);
+}
+
+void mcp23017_write_pin(uint8_t p, uint8_t d)
+{
+	uint8_t gpio;
+	uint8_t gpioaddr, olataddr;
+	uint8_t buf[2];
+
+	if (p > 15)
+		return;
+
+	if (p < 8) {
+		olataddr = MCP23017_OLATA;
+		gpioaddr = MCP23017_GPIOA;
+	} else {
+		olataddr = MCP23017_OLATB;
+		gpioaddr = MCP23017_GPIOB;
+		p -= 8;
+	}
+
+	/* Read the current GPIO output latches */
+	i2c_write_read(i2c_dev, MCP23017_ADDRESS, &olataddr, 1, &gpio, 1);
+
+	if (d == HIGH) {
+		gpio |= 1 << p;
+	} else {
+		gpio &= ~(1 << p);
+	}
+
+	buf[0] = gpioaddr;
+	buf[1] = gpio;
+	i2c_write(i2c_dev, buf, 2, MCP23017_ADDRESS);
+}
+
+void mcp23017_pull_up(uint8_t p, uint8_t d)
+{
+	uint8_t gppu;
+	uint8_t gppuaddr;
+	uint8_t buf[2];
+
+	if (p > 15)
+		return;
+
+	if (p < 8)
+		gppuaddr = MCP23017_GPPUA;
+	else {
+		gppuaddr = MCP23017_GPPUB;
+		p -= 8;
+	}
+
+	i2c_write_read(i2c_dev, MCP23017_ADDRESS, &gppuaddr, 1, &gppu, 1);
+
+	if (d == HIGH) {
+		gppu |= 1 << p;
+	} else {
+		gppu &= ~(1 << p);
+	}
+
+	buf[0] = gppuaddr;
+	buf[1] = gppu;
+	i2c_write(i2c_dev, buf, 2, MCP23017_ADDRESS);
+}
+
+int mcp23017_read_pin(uint8_t p, uint8_t *ret)
+{
+	uint8_t gpioaddr;
+	uint8_t rd_buf = 0;
+
+	if (p > 15)
+		return -1;
+
+	if (p < 8)
+		gpioaddr = MCP23017_GPIOA;
+	else {
+		gpioaddr = MCP23017_GPIOB;
+		p -= 8;
+	}
+
+	i2c_write_read(i2c_dev, MCP23017_ADDRESS, &gpioaddr, 1, &rd_buf, 1);
+
+	*ret = (rd_buf >> p) & 0x1;
+
+	return 0;
+}
